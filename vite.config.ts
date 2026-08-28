ab44de8ce95da25d97e597a7004a368f42131458
Аян Джуманалиева <aandzumanalieva@MacBook-Air-Aan.local>
2026-08-28 15:53:20 +0600

    Fix Sites static asset layout

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(() => {
       },
       plugins: [react(), sites()],
       build: {
+        outDir: 'dist/client',
         rollupOptions: {
           output: {
             manualChunks(id: string) {
